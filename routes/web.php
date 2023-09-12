8a681accc54c646984128004528e5a48f98c6af8
dipu157 <dipu157@gmail.com>
2023-09-12 16:22:08 +0600

    minimum authentication

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('auth.logi'); });
+Route::get('/', function () { return view('auth.login'); });
 
-Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
 
-Route::post('register', [RegisteredUserController::class, 'store']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+                ->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
