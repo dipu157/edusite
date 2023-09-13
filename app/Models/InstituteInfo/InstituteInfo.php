bfd40bd1c4d12c9e2306c4ae2bf92cf65dedaa28
dipu157 <dipu157@gmail.com>
2023-09-13 13:12:24 +0600

    some migration and model created

---
+++ app/Models/InstituteInfo/InstituteInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\InstituteInfo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InstituteInfo extends Model
+{
+    use HasFactory;
+}
